b3d085bd1bcb6651fca1f73a677f3de570090ae7
UMUTONIRitha <rithamarie9@gmail.com>
2020-12-10 16:00:16 +0200

    add changes

--- manage.py
+++ manage.py
@@ -4,13 +4,21 @@ from app.models import User
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('development')
+# app = create_app('development')
+app = create_app('test')
 manager = Manager(app)
 migrate = Migrate(app,db)
 
 manager.add_command('db',MigrateCommand)
 manager.add_command('server',Server)
 
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 @manager.shell
